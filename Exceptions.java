b651cb55694774fa6cba7324a7a32d7d1c42af81
Col-R <2colewestin@gmail.com>
2021-11-03 18:11:41 -0500

    created ArrayList

---
+++ Exceptions.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class Exceptions {
+    public static void main(String[] args) {
+        ArrayList<Object> myList = new ArrayList<Object>();
+        myList.add("13");
+        myList.add("hello world");
+        myList.add(48);
+        myList.add("Goodbye World");
+    }
+    
+}
